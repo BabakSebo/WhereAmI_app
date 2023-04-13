d32c2107492e30d0867524ecd72dae7b46b38a19
BabakSebo <bobsarjo@hotmail.com>
2023-04-13 18:11:10 +0100

    day two locations added to data and amended object keys

--- data/ImageData.js
+++ data/ImageData.js
@@ -1,7 +1,15 @@
 export const locationDayOne = {
-    one: {photo: require('../images/Ldn.jpeg'), answer: 'london', index: 1},
-    two: {photo: require('../images/Par.jpeg'), answer: 'paris', index: 2},
-    three: {photo: require('../images/Kmp.jpeg'), answer: 'kampala', index: 3},
-    four: {photo: require('../images/Taj.jpeg'), answer: 'taj mahal', index: 4},
-    five: {photo: require('../images/Mnt_Rsm.jpeg'), answer: 'mount rushmore', index: 5}
+    london: {photo: require('../images/Ldn.jpeg'), answer: 'london', index: 1},
+    paris: {photo: require('../images/Par.jpeg'), answer: 'paris', index: 2},
+    kampala: {photo: require('../images/Kmp.jpeg'), answer: 'kampala', index: 3},
+    agra: {photo: require('../images/Taj.jpeg'), answer: 'agra', index: 4},
+    keystone: {photo: require('../images/Mnt_Rsm.jpeg'), answer: 'keystone', index: 5}
+}
+
+export const locationDayTwo = {
+    barcelona: {photo: require('../images/Bcn.jpeg'), answer: 'barcelona', index: 1},
+    milan: {photo: require('../images/Mln.jpeg'), answer: 'milan', index: 2},
+    cape_town: {photo: require('../images/CpTw.jpeg'), answer: 'cape town', index: 3},
+    rio: {photo: require('../images/Rio.jpeg'), answer: 'rio de janeiro', index: 4},
+    giza: {photo: require('../images/Giza.jpeg'), answer: 'giza', index: 5}
 }
